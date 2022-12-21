2d45236b874a0536329a487e72b228b21c26eae1
Makayla <makaylabusher@gmail.com>
2022-12-21 14:02:26 -0600

    create show action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,9 @@ class PetsController < ApplicationController
     @pets = Pet.all
     render template: "pets/index"
   end
+
+  def show 
+    @pet = Pet.find_by(id: params[:id])
+    render template: "photos/show"
+  end
 end
