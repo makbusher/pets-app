bc5c6ee79e7a6d2236e49bb1033b8afc1b66f7b3
Makayla <makaylabusher@gmail.com>
2022-12-21 14:06:28 -0600

    create new/create action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,19 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: params[:id])
     render template: "photos/show"
   end
+
+  def new
+    @pet = Pet.new
+    render template: "pets/new"
+  end
+
+  def create
+    @pet = Pet.new(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    @pet.save
+    redirect_to "/pets"
+  end
 end
